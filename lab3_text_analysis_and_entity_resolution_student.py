b359bb4b2d6ec246ff42cd6f5fe2c95eae48f1d8
Echo <furong_g@163.com>
2019-10-04 15:56:56 +0200

    Create lab3_text_analysis_and_entity_resolution_student.py

---
+++ lab3_text_analysis_and_entity_resolution_student.py
@@ -0,0 +1,32 @@
+import re
+DATAFILE_PATTERN = '^(.+),"(.+)",(.*),(.*),(.*)'
+
+def removeQuotes(s):
+    """ Remove quotation marks from an input string
+    Args:
+        s (str): input string that might have the quote "" characters
+    Returns:
+        str: a string without the quote characters
+    """
+    return ''.join(i for i in s if i!='"')
+
+
+def parseDatafileLine(datafileLine):
+    """ Parse a line of the data file using the specified regular expression pattern
+    Args:
+        datafileLine (str): input string that is a line from the data file
+    Returns:
+        str: a string parsed using the given regular expression and without the quote characters
+    """
+    match = re.search(DATAFILE_PATTERN, datafileLine)
+    if match is None:
+        print 'Invalid datafile line: %s' % datafileLine
+        return (datafileLine, -1)
+    elif match.group(1) == '"id"':
+        print 'Header datafile line: %s' % datafileLine
+        return (datafileLine, 0)
+    else:
+        product = '%s %s %s' % (match.group(2), match.group(3), match.group(4))
+        return ((removeQuotes(match.group(1)), product), 1)
+        
+ 
