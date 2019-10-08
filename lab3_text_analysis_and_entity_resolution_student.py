988b9a32c5d5cd624b4a7558f71f11d6a3e8f128
Echo <furong_g@163.com>
2019-10-08 14:39:06 +0200

    Update lab3_text_analysis_and_entity_resolution_student.py

--- lab3_text_analysis_and_entity_resolution_student.py
+++ lab3_text_analysis_and_entity_resolution_student.py
@@ -28,5 +28,377 @@ def parseDatafileLine(datafileLine):
     else:
         product = '%s %s %s' % (match.group(2), match.group(3), match.group(4))
         return ((removeQuotes(match.group(1)), product), 1)
+    
+import sys
+import os
+from test_helper import Test
+
+baseDir = os.path.join('data')
+inputPath = os.path.join('cs100', 'lab3')
+
+GOOGLE_PATH = 'Google.csv'
+GOOGLE_SMALL_PATH = 'Google_small.csv'
+AMAZON_PATH = 'Amazon.csv'
+AMAZON_SMALL_PATH = 'Amazon_small.csv'
+GOLD_STANDARD_PATH = 'Amazon_Google_perfectMapping.csv'
+STOPWORDS_PATH = 'stopwords.txt'
+
+def parseData(filename):
+    """ Parse a data file
+    Args:
+        filename (str): input file name of the data file
+    Returns:
+        RDD: a RDD of parsed lines
+    """
+    return (sc
+            .textFile(filename, 4, 0)
+            .map(parseDatafileLine)
+            .cache())
+
+def loadData(path):
+    """ Load a data file
+    Args:
+        path (str): input file name of the data file
+    Returns:
+        RDD: a RDD of parsed valid lines
+    """
+    filename = os.path.join(baseDir, inputPath, path)
+    raw = parseData(filename).cache()
+    failed = (raw
+              .filter(lambda s: s[1] == -1)
+              .map(lambda s: s[0]))
+    for line in failed.take(10):
+        print '%s - Invalid datafile line: %s' % (path, line)
+    valid = (raw
+             .filter(lambda s: s[1] == 1)
+             .map(lambda s: s[0])
+             .cache())
+    print '%s - Read %d lines, successfully parsed %d lines, failed to parse %d lines' % (path,
+                                                                                        raw.count(),
+                                                                                        valid.count(),
+                                                                                        failed.count())
+    assert failed.count() == 0
+    assert raw.count() == (valid.count() + 1)
+    return valid
+
+googleSmall = loadData(GOOGLE_SMALL_PATH)
+google = loadData(GOOGLE_PATH)
+amazonSmall = loadData(AMAZON_SMALL_PATH)
+amazon = loadData(AMAZON_PATH)
+
+
+#remove stopwords
+def tokenize(string):
+    """ An implementation of input string tokenization that excludes stopwords
+    Args:
+        string (str): input string
+    Returns:
+        list: a list of tokens without stopwords
+    """
+    return [x for x in simpleTokenize(string) if x not in stopwords]
+
+#tf,idf
+def tf(tokens):
+    """ Compute TF
+    Args:
+        tokens (list of str): input list of tokens from tokenize
+    Returns:
+        dictionary: a dictionary of tokens to its TF values
+    """
+    d = {}
+    for x in tokens:
+        if x in d:
+            d[x] = d[x] + 1
+        else:
+            d[x] = 1
+    for y in d:
+        d[y] = d[y]/float(len(tokens))
+    return d
+
+def idfs(corpus):
+    """ Compute IDF
+    Args:
+        corpus (RDD): input corpus
+    Returns:
+        RDD: a RDD of (token, IDF value)
+    """
+    N = corpus.count()
+    uniqueTokens = corpus.flatMap(lambda x:set(x[1]))
+    tokenCountPairTuple = uniqueTokens.map(lambda x:(x,1))
+    tokenSumPairTuple = tokenCountPairTuple.reduceByKey(lambda x,y:x+y)
+    return (tokenSumPairTuple.map(lambda x:(x[0], float(N)/x[1])))
+
+#Implement a TF-IDF function
+
+def tfidf(tokens, idfs):
+    """ Compute TF-IDF
+    Args:
+        tokens (list of str): input list of tokens from tokenize
+        idfs (dictionary): record to IDF value
+    Returns:
+        dictionary: a dictionary of records to TF-IDF values
+    """
+    def tf(tokens):
+        d = {}
+        for x in tokens:
+            if x in d:
+                d[x] = d[x] + 1
+            else:
+                d[x] = 1
+        for y in d:
+            d[y] = d[y]/float(len(tokens))
+        return d
+    tfs = tf(tokens)
+    tfIdfDict = {}
+    for token, tf in tfs.items():
+        tfIdfDict[token] = tf * idfs[token]
+    return tfIdfDict
+
+#a cosineSimilarity function
+import math
+
+def dotprod(a, b):
+    """ Compute dot product
+    Args:
+        a (dictionary): first dictionary of record to value
+        b (dictionary): second dictionary of record to value
+    Returns:
+        dotProd: result of the dot product with the two input dictionaries
+    """
+    result = 0
+    for x in a:
+        if x in b:
+            result = result + a[x]*b[x]
+    return result
+
+def norm(a):
+    """ Compute square root of the dot product
+    Args:
+        a (dictionary): a dictionary of record to value
+    Returns:
+        norm: a dictionary of tokens to its TF values
+    """
+    result = 0
+    for x in a:
+        result = result + a[x]*a[x]
+    result = math.sqrt(result)
+    return result
+
+def cossim(a, b):
+    """ Compute cosine similarity
+    Args:
+        a (dictionary): first dictionary of record to value
+        b (dictionary): second dictionary of record to value
+    Returns:
+        cossim: dot product of two dictionaries divided by the norm of the first dictionary and
+                then by the norm of the second dictionary
+    """
+    return dotprod(a,b)/(norm(a)*norm(b))
+
+def cosineSimilarity(string1, string2, idfsDictionary):
+    """ Compute cosine similarity between two strings
+    Args:
+        string1 (str): first string
+        string2 (str): second string
+        idfsDictionary (dictionary): a dictionary of IDF values
+    Returns:
+        cossim: cosine similarity value
+    """
+    w1 = tfidf(tokenize(string1),idfsDictionary)
+    w2 = tfidf(tokenize(string2),idfsDictionary)
+    return cossim(w1, w2)
+
+
+def computeSimilarityBroadcast(record):
+    """ Compute similarity on a combination record, using Broadcast variable
+    Args:
+        record: a pair, (google record, amazon record)
+    Returns:
+        pair: a pair, (google URL, amazon ID, cosine similarity value)
+    """
+    googleRec = record[0]
+    amazonRec = record[1]
+    googleURL = googleRec[0]
+    amazonID = amazonRec[0]
+    googleValue = googleRec[1]
+    amazonValue = amazonRec[1]
+    cs = cosineSimilarity(googleValue, amazonValue, idfsSmallBroadcast.value)
+    return (googleURL, amazonID, cs)
+
+def similarBroadcast(amazonID, googleURL):
+    """ Return similarity value, computed using Broadcast variable
+    Args:
+        amazonID: amazon ID
+        googleURL: google URL
+    Returns:
+        similar: cosine similarity value
+    """
+    return (similaritiesBroadcast
+            .filter(lambda record: (record[0] == googleURL and record[1] == amazonID))
+            .collect()[0][2])
+def invert(record):
+    """ Invert (ID, tokens) to a list of (token, ID)
+    Args:
+        record: a pair, (ID, token vector)
+    Returns:
+        pairs: a list of pairs of token to ID
+    """
+    output = []
+    for x in record[1]:
+        output.append((x, record[0]))
+    return output
+def fastCosineSimilarity(record):
+    """ Compute Cosine Similarity using Broadcast variables
+    Args:
+        record: ((ID, URL), token)
+    Returns:
+        pair: ((ID, URL), cosine similarity value)
+    """
+    amazonRec = record[0][0]
+    googleRec = record[0][1]
+    tokens = record[1]
+    s = sum([amazonWeightsBroadcast.value.get(amazonRec, {}).get(t, 0) * googleWeightsBroadcast.value.get(googleRec, {}).get(t, 0) for t in tokens])
+    value = s / float(amazonNormsBroadcast.value[amazonRec] * googleNormsBroadcast.value[googleRec])
+    key = (amazonRec, googleRec)
+    return (key, value)
+
+
+
+
+amazonFullRecToToken = amazon.map(lambda x:(x[0], tokenize(x[1])))
+googleFullRecToToken = google.map(lambda x:(x[0], tokenize(x[1])))
+
+fullCorpusRDD = amazonFullRecToToken.union(googleFullRecToToken)
+idfsFull = idfs(fullCorpusRDD)
+
+idfsFullWeights = idfsFull.collectAsMap()
+idfsFullBroadcast = sc.broadcast(idfsFullWeights)
+
+amazonWeightsRDD = amazonFullRecToToken.map(lambda x: (x[0], tfidf(x[1], idfsFullWeights)))
+googleWeightsRDD = googleFullRecToToken.map(lambda x: (x[0], tfidf(x[1], idfsFullWeights)))
+
+amazonNorms = amazonWeightsRDD.map(lambda x:(x[0], norm(x[1]))).collectAsMap()
+amazonNormsBroadcast = sc.broadcast(amazonNorms)
+googleNorms = googleWeightsRDD.map(lambda x:(x[0], norm(x[1]))).collectAsMap()
+googleNormsBroadcast = sc.broadcast(googleNorms)
+
+amazonInvPairsRDD = (amazonWeightsRDD
+                    .map(lambda x:(x[0], x[1].keys()))
+                    .flatMap(lambda x:invert(x))
+                    .cache())
+googleInvPairsRDD = (googleWeightsRDD
+                    .map(lambda x:(x[0], x[1].keys()))
+                    .flatMap(lambda x:invert(x))
+                    .cache())
+
+amazonWeightsBroadcast = sc.broadcast(amazonWeightsRDD.collectAsMap())
+googleWeightsBroadcast = sc.broadcast(googleWeightsRDD.collectAsMap())
+
+similaritiesFullRDD = (commonTokens
+                       .map(lambda x:fastCosineSimilarity(x))
+                       .cache())
         
  
+
+# Create an RDD of ((Amazon ID, Google URL), similarity score)
+simsFullRDD = similaritiesFullRDD.map(lambda x: ("%s %s" % (x[0][0], x[0][1]), x[1]))
+assert (simsFullRDD.count() == 2441100)
+
+# Create an RDD of just the similarity scores
+simsFullValuesRDD = (simsFullRDD
+                     .map(lambda x: x[1])
+                     .cache())
+assert (simsFullValuesRDD.count() == 2441100)
+
+# Look up all similarity scores for true duplicates
+
+# This helper function will return the similarity score for records that are in the gold standard and the simsFullRDD (True positives), and will return 0 for records that are in the gold standard but not in simsFullRDD (False Negatives).
+def gs_value(record):
+    if (record[1][1] is None):
+        return 0
+    else:
+        return record[1][1]
+
+# Join the gold standard and simsFullRDD, and then extract the similarities scores using the helper function
+trueDupSimsRDD = (goldStandard
+                  .leftOuterJoin(simsFullRDD)
+                  .map(gs_value)
+                  .cache())
+print 'There are %s true duplicates.' % trueDupSimsRDD.count()
+assert(trueDupSimsRDD.count() == 1300)
+
+from pyspark.accumulators import AccumulatorParam
+class VectorAccumulatorParam(AccumulatorParam):
+    # Initialize the VectorAccumulator to 0
+    def zero(self, value):
+        return [0] * len(value)
+
+    # Add two VectorAccumulator variables
+    def addInPlace(self, val1, val2):
+        for i in xrange(len(val1)):
+            val1[i] += val2[i]
+        return val1
+
+# Return a list with entry x set to value and all other entries set to 0
+def set_bit(x, value, length):
+    bits = []
+    for y in xrange(length):
+        if (x == y):
+          bits.append(value)
+        else:
+          bits.append(0)
+    return bits
+
+# Pre-bin counts of false positives for different threshold ranges
+BINS = 101
+nthresholds = 100
+def bin(similarity):
+    return int(similarity * nthresholds)
+
+# fpCounts[i] = number of entries (possible false positives) where bin(similarity) == i
+zeros = [0] * BINS
+fpCounts = sc.accumulator(zeros, VectorAccumulatorParam())
+
+def add_element(score):
+    global fpCounts
+    b = bin(score)
+    fpCounts += set_bit(b, 1, BINS)
+
+simsFullValuesRDD.foreach(add_element)
+
+# Remove true positives from FP counts
+def sub_element(score):
+    global fpCounts
+    b = bin(score)
+    fpCounts += set_bit(b, -1, BINS)
+
+trueDupSimsRDD.foreach(sub_element)
+
+def falsepos(threshold):
+    fpList = fpCounts.value
+    return sum([fpList[b] for b in range(0, BINS) if float(b) / nthresholds >= threshold])
+
+def falseneg(threshold):
+    return trueDupSimsRDD.filter(lambda x: x < threshold).count()
+
+def truepos(threshold):
+    return trueDupSimsRDD.count() - falsenegDict[threshold]
+
+# Precision = true-positives / (true-positives + false-positives)
+# Recall = true-positives / (true-positives + false-negatives)
+# F-measure = 2 x Recall x Precision / (Recall + Precision)
+
+def precision(threshold):
+    tp = trueposDict[threshold]
+    return float(tp) / (tp + falseposDict[threshold])
+
+def recall(threshold):
+    tp = trueposDict[threshold]
+    return float(tp) / (tp + falsenegDict[threshold])
+
+def fmeasure(threshold):
+    r = recall(threshold)
+    p = precision(threshold)
+    return 2 * r * p / (r + p)
+
+
